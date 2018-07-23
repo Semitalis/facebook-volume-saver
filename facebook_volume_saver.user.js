447b9dd636da0995f558e5d5d54d1dbe8a1375b2
Kerim <kerim.merdenoglu@gmail.com>
2018-07-23 17:13:55 +0200

    cleaned up code a little

--- facebook_volume_saver.user.js
+++ facebook_volume_saver.user.js
@@ -5,7 +5,7 @@
 // @license      MIT
 // @author       Semitalis
 // @namespace    https://github.com/Semitalis/
-// @version      1.1
+// @version      1.2
 // @homepage     https://github.com/Semitalis/facebook-volume-saver
 // @downloadURL	 https://raw.githubusercontent.com/Semitalis/facebook-volume-saver/master/facebook_volume_saver.user.js
 // @updateURL    https://raw.githubusercontent.com/Semitalis/facebook-volume-saver/master/facebook_volume_saver.user.js
@@ -13,6 +13,8 @@
 // ==/UserScript==
 /*
 Changelog:
+1.2:
+- cleaned up code a little
 1.1:
 - sync volume to other video elements
 1.0:
@@ -20,42 +22,44 @@ Changelog:
 */
 
 // UTIL FUNCS
-function isNumber(v) {
-    return !isNaN(parseFloat(v)) && !isNaN(v - 0);
-}
-
-function isFloat(v) {
-    return (v === +v) && (v !== (v|0));
-}
-
-function isInteger(v) {
-    return (v === +v) && (v === (v|0));
-}
-
-function isBool(v){
-    return typeof v === 'boolean';
-}
-
-function storage_save(key, value){
-    localStorage[key] = value;
-}
-
-function storage_load(key, def){
-    var v = localStorage[key];
-    if(!v){
-        return def;
-    }
-    if(isFloat(def)){
-        try{ v = parseFloat(v); } catch(e){ return def; }
-    }
-    if(isInteger(def)){
-        try{ v = parseInt(v); } catch(e){ return def; }
-    }
-    if(isBool(def)){
-        return v === "true";
-    }
-    return v;
-}
+var semi_utils = {
+    isNumber : function(v) {
+        return !isNaN(parseFloat(v)) && !isNaN(v - 0);
+    },
+    isFloat : function (v) {
+        return (v === +v) && (v !== (v|0));
+    },
+    isInteger : function (v) {
+        return (v === +v) && (v === (v|0));
+    },
+    isBool : function (v){
+        return typeof v === 'boolean';
+    },
+    storage : (function(){
+        var storage = localStorage;
+        return {
+            save : function (key, value){
+                storage[key] = value;
+            },
+            load : function (key, def){
+                var v = storage[key];
+                if(!v){
+                    return def;
+                }
+                if(semi_utils.isFloat(def)){
+                    try{ v = parseFloat(v); } catch(e){ return def; }
+                }
+                if(semi_utils.isInteger(def)){
+                    try{ v = parseInt(v); } catch(e){ return def; }
+                }
+                if(semi_utils.isBool(def)){
+                    return v === "true";
+                }
+                return v;
+            }
+        };
+    }())
+};
 
 // MAIN
 (function() {
@@ -63,22 +67,28 @@ function storage_load(key, def){
 
     // PRIVATE VARIABLES
     var m = {
+        debug    : true,
         observer : null,
-        volume   : storage_load('semi_video_volume', 0.25),
-        muted    : storage_load('semi_video_muted', false),
+        volume   : semi_utils.storage.load('semi_video_volume', 0.25),
+        muted    : semi_utils.storage.load('semi_video_muted', false),
         current  : null
     };
 
     // PRIVATE METHODS
     var f = {
+        log : function(s){
+            if(m.debug){
+                console.log(s);
+            }
+        },
         check : function(nodes){
             var node;
             for(node of nodes){
                 if(node.nodeName === 'VIDEO'){
-                    //node.addEventListener('canplay', function(){
-                    //    this.volume = m.volume;
-                    //    this.semi_init = true;
-                    //});
+                    if(node.semi_init === true){
+                        return;
+                    }
+                    node.semi_init = true;
                     node.addEventListener('play', function(){
                         if(m.current === this){
                             return;
@@ -86,7 +96,7 @@ function storage_load(key, def){
                         this.volume = m.volume;
                         this.muted = m.muted;
                         m.current = this;
-                        //console.log('changed player: ' + m.current.id);
+                        f.log("[FVS] changed active player: '" + m.current.id + "'");
                     });
                     node.addEventListener('volumechange', function(){
                         var v;
@@ -94,16 +104,16 @@ function storage_load(key, def){
                             return;
                         }
                         v = this.volume;
-                        if(isNumber(v) && (v !== m.volume)){
+                        if(semi_utils.isNumber(v) && (v !== m.volume)){
                             m.volume = v;
-                            //console.log('volume changed: ' + this.volume);
-                            storage_save('semi_video_volume', this.volume);
+                            f.log("[FVS] volume changed: " + this.volume);
+                            semi_utils.storage.save('semi_video_volume', this.volume);
                         }
                         v = this.muted;
-                        if(isBool(v) && (v !== m.muted)){
+                        if(semi_utils.isBool(v) && (v !== m.muted)){
                             m.muted = v;
-                            //console.log('muted changed: ' + this.muted);
-                            storage_save('semi_video_muted', this.muted);
+                            f.log("[FVS] muted changed: " + this.muted);
+                            semi_utils.storage.save('semi_video_muted', this.muted);
                         }
                     });
                 }
@@ -111,7 +121,7 @@ function storage_load(key, def){
             }
         },
         init : function(){
-            //console.log("[FVS] initialized. volume(" + m.volume + "), muted(" + m.muted + ")");
+            f.log("[FVS] initialized. volume(" + m.volume + "), muted(" + m.muted + ")");
 
             // setup observer for new DOM elements
             m.observer = new MutationObserver(function(mutations) {
