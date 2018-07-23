79fd426e52f133957c5afe731125f91f2164a096
Kerim <kerim.merdenoglu@gmail.com>
2018-07-23 04:13:56 +0200

    sync volume to other video elements

--- facebook_volume_saver.user.js
+++ facebook_volume_saver.user.js
@@ -5,33 +5,68 @@
 // @license      MIT
 // @author       Semitalis
 // @namespace    https://github.com/Semitalis/
-// @version      1.0
+// @version      1.1
 // @homepage     https://github.com/Semitalis/facebook-volume-saver
 // @downloadURL	 https://raw.githubusercontent.com/Semitalis/facebook-volume-saver/master/facebook_volume_saver.user.js
 // @updateURL    https://raw.githubusercontent.com/Semitalis/facebook-volume-saver/master/facebook_volume_saver.user.js
 // @grant        none
 // ==/UserScript==
+/*
+Changelog:
+1.1:
+- sync volume to other video elements
+1.0:
+- initial version
+*/
 
+// UTIL FUNCS
+function isNumber(v) {
+    return !isNaN(parseFloat(v)) && !isNaN(v - 0);
+}
+
+function isFloat(v) {
+    return (v === +v) && (v !== (v|0));
+}
+
+function isInteger(v) {
+    return (v === +v) && (v === (v|0));
+}
+
+function isBool(v){
+    return typeof v === 'boolean';
+}
+
+function storage_save(key, value){
+    localStorage[key] = value;
+}
+
+function storage_load(key, def){
+    var v = localStorage[key];
+    if(!v){
+        return def;
+    }
+    if(isFloat(def)){
+        try{ v = parseFloat(v); } catch(e){ return def; }
+    }
+    if(isInteger(def)){
+        try{ v = parseInt(v); } catch(e){ return def; }
+    }
+    if(isBool(def)){
+        return v === "true";
+    }
+    return v;
+}
+
+// MAIN
 (function() {
     'use strict';
-
-    // UTIL FUNCS
-    function isNumber(v) {
-        return !isNaN(parseFloat(v)) && !isNaN(v - 0);
-    }
-
-    function storage_save(key, value){
-        localStorage[key] = value;
-    }
-
-    function storage_load(key, def){
-        return localStorage[key] || def;
-    }
 
     // PRIVATE VARIABLES
     var m = {
         observer : null,
-        volume   : storage_load('semi_video_volume', 0.25)
+        volume   : storage_load('semi_video_volume', 0.25),
+        muted    : storage_load('semi_video_muted', false),
+        current  : null
     };
 
     // PRIVATE METHODS
@@ -40,15 +75,35 @@
             var node;
             for(node of nodes){
                 if(node.nodeName === 'VIDEO'){
-                    node.addEventListener('canplay', function(){
+                    //node.addEventListener('canplay', function(){
+                    //    this.volume = m.volume;
+                    //    this.semi_init = true;
+                    //});
+                    node.addEventListener('play', function(){
+                        if(m.current === this){
+                            return;
+                        }
                         this.volume = m.volume;
+                        this.muted = m.muted;
+                        m.current = this;
+                        //console.log('changed player: ' + m.current.id);
                     });
                     node.addEventListener('volumechange', function(){
-                        var v = this.volume;
+                        var v;
+                        if(m.current != this){
+                            return;
+                        }
+                        v = this.volume;
                         if(isNumber(v) && (v !== m.volume)){
                             m.volume = v;
                             //console.log('volume changed: ' + this.volume);
                             storage_save('semi_video_volume', this.volume);
+                        }
+                        v = this.muted;
+                        if(isBool(v) && (v !== m.muted)){
+                            m.muted = v;
+                            //console.log('muted changed: ' + this.muted);
+                            storage_save('semi_video_muted', this.muted);
                         }
                     });
                 }
@@ -56,6 +111,8 @@
             }
         },
         init : function(){
+            //console.log("[FVS] initialized. volume(" + m.volume + "), muted(" + m.muted + ")");
+
             // setup observer for new DOM elements
             m.observer = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutation) {
@@ -70,5 +127,5 @@
     };
 
     // initialize framework
-    f.init();
+    window.addEventListener('load', f.init);
 })();
